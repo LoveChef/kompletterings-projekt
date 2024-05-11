b2ac69b2543012ada08a2c76316b2f956009aea9
LoveChef <117826056+LoveChef@users.noreply.github.com>
2024-05-11 22:04:19 +0200

    api, samt hämtning av  informatio ntill film

--- main.py
+++ main.py
@@ -5,7 +5,27 @@ class API:
         self.api_key = api_key
         api_key = "52642a868038e1153406d79c42346aae"
 
-    def apidata(self, search):
+    def apidata(self, search, type):
         url = f"https://api.themoviedb.org/3/search/{type}?api_key={self.api_key}&query={search}"
         response = requests.get(url)
         data = response.json()
+        
+        result = []
+        for item in data["results"]:
+            if type == "movie":
+                result.append(Movie(item["title"], item["id"]))
+        return result
+
+class Movie:
+    def __init__(self, api_key, title, id):
+        self.api_key = api_key
+        self.titel = title
+
+    def movie_info(self):
+        url = f"https://api.themoviedb.org/3/movie/{type}?api_key={self.api_key}"
+        response = requests.get(url)
+        data = response.json()
+
+        print(f"Info om: {self.titel}")
+        print(f"Beskrivning: {data["overview"]}")
+        print(f"Betyg: {data["vote_average"]}")
