6ed4ba59a534b9634d073a3afda5105ce35731d3
LoveChef <117826056+LoveChef@users.noreply.github.com>
2024-05-11 22:19:02 +0200

    använde mig av fel variabler för att söka film, behöver ej id

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ class API:
         result = []
         for item in data["results"]:
             if type == "movie":
-                result.append(Movie(item["title"], item["id"]))
+                result.append(Movie(item["title"]))
         return result
 
 class Movie:
-    def __init__(self, api_key, title, id):
+    def __init__(self, api_key, title):
         self.api_key = api_key
         self.titel = title
 
     def movie_info(self):
-        url = f"https://api.themoviedb.org/3/movie/{type}?api_key={self.api_key}"
+        url = f"https://api.themoviedb.org/3/movie/?api_key={self.api_key}&query={search}"
         response = requests.get(url)
         data = response.json()
 
         print(f"Info om: {self.titel}")
-        print(f"Beskrivning: {data["overview"]}")
-        print(f"Betyg: {data["vote_average"]}")
+        print(f"Beskrivning: {data['overview']}")
+        print(f"Betyg: {data['vote_average']}")
